2431c4d3f95f06f12ff926dc2d0b5470b52be64a
Shresth Dwivedi <shresth0109@gmail.com>
2024-06-24 18:45:42 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## TODO App
+
+This project contains a simple TODO application.
+It has the following features:
+
+- Anyone can create a todo
+- Anyone can see their existing todo
+- Anyone can mark a todo as done
---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+app.use(express.json());
+
+app.post("/todo", function(req,res){
+
+})
+
+app.get("/todo", function(req,res){
+    
+})
+
+app.put("/completed", function(req,res){
+    
+})
