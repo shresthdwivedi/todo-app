7b1d6dc752e1b260f08c42d9902508263b6e23f5
Shresth Dwivedi <shresth0109@gmail.com>
2024-06-24 23:00:57 +0530

    added db logic

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://shresthtech10:*****@cluster0.nwaqnqa.mongodb.net/todos");
+
+const todoSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+
+})
+
+const todo = mongoose.model('todos', todoschema);
+
+module.exports = {
+    todo
+}
--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,14 @@ const app = express();
 app.use(express.json());
 
 app.post("/todo", function(req,res){
-
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
 
 app.get("/todo", function(req,res){
@@ -13,5 +20,12 @@ app.get("/todo", function(req,res){
 })
 
 app.put("/completed", function(req,res){
-    
+    const updatePayload = req.body;
+    const parsedPayload = createTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
