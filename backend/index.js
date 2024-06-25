8ba8408ebabc4c63ee112f1c89fbe32a12240c18
Shresth Dwivedi <shresth0109@gmail.com>
2024-06-25 12:42:32 +0530

    add cors package

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
-const express = require("express");
-const { createTodo, updateTodo } = require("./types");
-const { todo } = require("./db");
+const express = require('express');
+const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 
 app.post("/todo", async function(req,res){
     const createPayload = req.body;
